877023a1bc07407e6d9cc1f9f6c57267f48bb9c4
ratslove <122135063+ratslove@users.noreply.github.com>
2023-02-11 11:57:12 -0500

    Create DH.lua

---
+++ DH.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 2788229376 then
+        local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/bruvzz/ducklibrary/main/src.lua")()
+    
+        local win = lib:Window("Oasis Hub - Da Hood",Color3.fromRGB(255, 156, 107), Enum.KeyCode)
+        
+        local maintab = win:Tab("Main")
+        
+        maintab:Label('This script is outdated, sorry!')
+  end
