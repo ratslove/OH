a9cd71d754090f550eb61d68281d1f91630b2022
ratslove <122135063+ratslove@users.noreply.github.com>
2023-02-11 18:59:41 -0500

    Update PL.lua

--- PL.lua
+++ PL.lua
@@ -630,27 +630,6 @@ end)
 
 
     end)
-    
-    misctab:Button('Arrest All', function()
-    
-        wait(0.1)
-        Player = game.Players.LocalPlayer
-        Pcf = Player.Character.HumanoidRootPart.CFrame
-        for i,v in pairs(game.Teams.Criminals:GetPlayers()) do
-        if v.Name ~= Player.Name then
-        local i = 10
-        repeat
-        wait()
-        i = i-1
-        game.Workspace.Remote.arrest:InvokeServer(v.Character.HumanoidRootPart)
-        Player.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, 1)
-        until i == 0
-    end
-    end
-
-    lib:Notification("Notification", "Arresting Criminals..", "Close") -- (header, text, closebutton) --
-        
-    end)
 
     misctab:Button('Builder Tools (BTools)', function()
     
