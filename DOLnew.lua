b9755012219bb8302ec015bbc61cc4566ceef84c
ratslove <122135063+ratslove@users.noreply.github.com>
2023-04-04 16:40:54 -0400

    Create DOLnew.lua

---
+++ DOLnew.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 6516141723 then
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/UI-Libraries/main/Vynixius/Source.lua"))()
+
+    Library:Notify({title = "Alert", text = "Please Execute in-game.", color = Color3.fromRGB(43, 153, 8)}, function(v)
+                    
+    end)
+end
